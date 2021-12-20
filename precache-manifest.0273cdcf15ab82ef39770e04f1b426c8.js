56326c4e1e23fae8167d1b21a7a467d85838c7ad
saintic <saintic132@gmail.com>
2021-12-20 22:28:19 +0300

    Updates

---
+++ precache-manifest.0273cdcf15ab82ef39770e04f1b426c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81738b07ea63694b92863eecdf1362e",
+    "url": "/TDHomeWork/index.html"
+  },
+  {
+    "revision": "08918c206f3beacde5b4",
+    "url": "/TDHomeWork/static/css/main.ea7513b5.chunk.css"
+  },
+  {
+    "revision": "5a7fac55f76d735ab332",
+    "url": "/TDHomeWork/static/js/2.24a43c10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TDHomeWork/static/js/2.24a43c10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08918c206f3beacde5b4",
+    "url": "/TDHomeWork/static/js/main.0eded5be.chunk.js"
+  },
+  {
+    "revision": "1a662f839cb521bf649d",
+    "url": "/TDHomeWork/static/js/runtime-main.f37395a6.js"
+  }
+]);
