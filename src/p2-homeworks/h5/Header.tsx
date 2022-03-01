c86b24b61125d9875b83e3ef1bb879cfafb7e8bb
saintic <saintic132@gmail.com>
2022-03-01 15:19:07 +0300

    Переделана работа ДЗ5 под flex

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,43 +1,36 @@
 import React from 'react'
 import s from './Header.module.css'
-import {NavLink} from "react-router-dom";
 
 function Header() {
     return (
         <div className={s.headerNames}>
-
-            <NavLink to='/'>
-                <div>
-                    Home
+            <div className={s.homeName}>
+                <h3>HOME</h3>
+            </div>
+            <div className={s.studyNames}>
+                <div className={s.preJunior}>
+                    <div className={s.preJuniorName}>
+                        <b>Pre-Junior</b>
+                    </div>
+                    <div className={s.preJuniorLessons}>
+                        <div>HomeWork 1</div>
+                        <div>HomeWork 2</div>
+                        <div>HomeWork 3</div>
+                        <div>HomeWork 4</div>
+                    </div>
                 </div>
-            </NavLink>
-
-            <NavLink to='/pre-junior'>
-                <div>
-                    1
+                <div className={s.junior}>
+                    <div className={s.juniorName}>
+                        <b>Junior</b>
+                    </div>
+                    <div className={s.juniorLessons}>
+                        <div>HomeWork 5</div>
+                        <div>HomeWork 6</div>
+                        <div>HomeWork 7</div>
+                        <div>HomeWork 8</div>
+                    </div>
                 </div>
-            </NavLink>
-
-            <NavLink to="/pre-junior">
-                <div>
-                    2
-                </div>
-            </NavLink>
-
-            <NavLink to="/pre-junior">
-                <div>
-                    3
-                </div>
-            </NavLink>
-
-            <NavLink to="/pre-junior">
-                <div>
-                    4
-                </div>
-            </NavLink>
-
-
-
+            </div>
         </div>
     )
 }
